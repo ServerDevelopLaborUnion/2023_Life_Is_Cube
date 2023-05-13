007a539dfe10ccabdbcd9881f3051e4e91e6f729
SEH00N <06psh0501@gmail.com>
2023-05-13 11:55:57 +0900

    auto targetting

--- RubiksCube/Assets/01. Scripts/State/AttackState.cs
+++ RubiksCube/Assets/01. Scripts/State/AttackState.cs
@@ -8,14 +8,31 @@ public class AttackState : State
         playerAnimator.ToggleAttack(true);
         playerMovement.StopImmediatly();
 
-        // playerMovement.IsActiveRotate = false;
-        
-        // Vector3 lookTarget = playerInput.GetMouseWorldPosition();
+        Collider[] enemies = Physics.OverlapSphere(transform.position, 100f, DEFINE.EnemyLayer);
+        Transform target = null;
+        float nearDistance = float.MaxValue;
 
-        // if(lookTarget.sqrMagnitude <= 0)
-        //     lookTarget = transform.forward + transform.position;
+        foreach (Collider enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < nearDistance)
+            {
+                nearDistance = distance;
+                target = enemy.transform;
+            }
+        }
 
-        // playerMovement.SetRotation(lookTarget);
+        if (target != null)
+        {
+            Vector3 lookTarget = target.position;
+            //playerMovement.IsActiveRotate = false;
+
+            if (lookTarget.sqrMagnitude <= 0)
+                lookTarget = transform.forward + transform.position;
+
+            playerMovement.SetRotationImmediatly(lookTarget);
+        }
+
 
         playerAnimator.OnAnimationEndTrigger += OnAnimationEndHandle;
     }
