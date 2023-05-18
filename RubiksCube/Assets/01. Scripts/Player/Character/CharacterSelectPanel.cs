ad75f4d1739e780bde6668994bbefedaf4a968f9
ahslxjvhapt12 <likjhee1@gmail.com>
2023-05-19 00:23:07 +0900

    CharacterSelectPanel

--- RubiksCube/Assets/01. Scripts/Player/Character/CharacterSelectPanel.cs
+++ RubiksCube/Assets/01. Scripts/Player/Character/CharacterSelectPanel.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelectPanel : MonoBehaviour
 {
@@ -13,10 +15,103 @@ public class CharacterSelectPanel : MonoBehaviour
         set { current = value; }
     }
 
+    [Header("UI")]
+    [SerializeField] private GameObject infoPanel;
+
+    #region Slider Group
+    private Slider hp;
+    private Slider speed;
+    private Slider critical;
+    #endregion
+
+    #region Icon Group
+    private Image skillIcon;
+    private Image classIcon;
+
+    [SerializeField] private Image descriptionSkillIcon;
+    #endregion
+
+    #region TextGroup
+    [SerializeField] private TextMeshProUGUI skillTitle;
+    [SerializeField] private TextMeshProUGUI description;
+    #endregion
+
+    private int idx = 0;
+
     private void Awake()
     {
+        // 슬라이더들
+        Transform gauge = infoPanel.transform.GetChild(0);
+
+        hp = gauge.GetChild(0).GetComponent<Slider>();
+        speed = gauge.GetChild(1).GetComponent<Slider>();
+        critical = gauge.GetChild(2).GetComponent<Slider>();
+
+        // 아이콘들
+        Transform IconPanel = infoPanel.transform.GetChild(1);
+
+        skillIcon = IconPanel.GetChild(0).GetComponent<Image>();
+        classIcon = IconPanel.GetChild(1).GetComponent<Image>();
+
+
+        // 자식에 등록된 캐릭터들을 리스트로
         characters = GetComponentsInChildren<Character>().ToList();
-        current = characters[0];
+        // 현재 직업을 나무꾼으로
+        current = characters[idx];
+        // UI 초기화
+        ChangeCurrent();
+    }
+
+    /// <summary>
+    /// 오른쪽 화살표 눌렀을때 다음직업
+    /// </summary>
+    public void MoveRight()
+    {
+        if (idx >= characters.Count - 1)
+        {
+            Debug.LogWarning("You are trying to access wrong index");
+            return;
+        }
+        current = characters[++idx];
+        ChangeCurrent();
+    }
+    
+    /// <summary>
+    /// 왼쪽 화살표 눌렀을때 이전직업
+    /// </summary>
+    public void MoveLeft()
+    {
+        if (idx <= 0)
+        {
+            Debug.LogWarning("You are trying to access wrong index");
+            return;
+        }
+        current = characters[--idx];
+        ChangeCurrent();
+    }
+
+    private void ChangeCurrent()
+    {
+
+        /*  
+        idx   type
+        0   : hp
+        1   : speed
+        2   : ad
+        3   : critical
+        */
+
+        hp.value = current.stats[0].value;
+        speed.value = current.stats[1].value;
+        critical.value = current.stats[3].value;
+
+        skillIcon.sprite = current.skillIcon;
+        descriptionSkillIcon.sprite = current.skillIcon;
+
+        classIcon.sprite = current.classIcon;
+
+        skillTitle.text = current.skillName;
+        description.text = current.skillDescription;
     }
 
     public void GameStart()
