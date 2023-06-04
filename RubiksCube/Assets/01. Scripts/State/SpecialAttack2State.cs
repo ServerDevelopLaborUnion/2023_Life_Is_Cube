535313f35f06608d20ac23d83e2bbd2605cb646e
ahslxjvhapt12 <likjhee1@gmail.com>
2023-06-05 00:25:34 +0900

    Input
    CoolTime

---
+++ RubiksCube/Assets/01. Scripts/State/SpecialAttack2State.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class SpecialAttack2State : State
+{
+    public override void OnStateEnter()
+    {
+        playerAnimator.ToggleSpecialAttack(true);
+        playerMovement.StopImmediatly();
+
+        // playerMovement.IsActiveRotate = false;
+
+        // Vector3 lookTarget = playerInput.GetMouseWorldPosition();
+
+        // if(lookTarget.sqrMagnitude <= 0)
+        //     lookTarget = transform.forward + transform.position;
+
+        // playerMovement.SetRotation(lookTarget);
+
+        playerAnimator.OnAnimationEndTrigger += OnAnimationEndHandle;
+    }
+
+    public override void StateUpdate()
+    {
+
+    }
+
+    public override void OnStateExit()
+    {
+        playerAnimator.ToggleAttack(false);
+
+        //playerMovement.IsActiveRotate = true;
+
+        playerAnimator.OnAnimationEndTrigger -= OnAnimationEndHandle;
+    }
+
+    private void OnAnimationEndHandle()
+    {
+        stateHandler.ChangeState(StateFlags.Normal);
+    }
+}
